92a5eb8e196bab1af1ef33df2f8f232464731e44
Sebastian Medina <snmedina91@gmail.com>
2023-12-27 09:05:42 -0300

    strapi test

---
+++ src/api/realstate/controllers/realstate.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * realstate controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::realstate.realstate');
---
+++ src/api/realstate/services/realstate.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * realstate service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::realstate.realstate');
